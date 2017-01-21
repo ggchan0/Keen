638fb6c93bff78e39fe6b0fa55e91b260d0626cc
Garrett <ggchan@calpoly.edu>
2017-01-21 11:31:13 -0800

    delete mode first revision

--- CPE-357/hw1/mytr.c
+++ CPE-357/hw1/mytr.c
@@ -5,25 +5,81 @@ int get_mode(int argc, char ** args) {
    int i;
    int code = 0;
    for (i = 0; i < argc; i++) {
-	char *arg = *(args + i);
-	printf("%s\n", arg);
-	if (strcmp(arg, "-d") == 0) {
-	   code = 1;
-	}
+      char *arg = *(args + i);
+      if (strcmp(arg, "-d") == 0) {
+        code = 1;
+      }
    }
    return code;
 }
 
+int char_in_delete_set(char c, char *delete_set) {
+   int i = 0;
+   int delete_char = 0;
+   while (delete_set[i] != '\0') {
+      if (delete_set[i] == '\\') {
+         i++;
+         switch(delete_set[i]) {
+            case '\0':
+               delete_char = (c == '\\');
+               break;
+
+            case '\\':
+               delete_char = (c == '\\');
+               break;
+
+            case 'n':
+               delete_char = (c == '\n');
+               break;
+            case 't':
+               delete_char = (c == '\t');
+               break;
+
+            default:
+               delete_char = (c == delete_set[i]);
+               break;
+         }
+      } else {
+         delete_char = (c == delete_set[i]);
+      }
+      if (delete_char > 0) {
+         break;
+      }
+      i++;
+   }
+   return delete_char;
+}
+
+char *get_delete_set(char ** argv) {
+   char *delete_set;
+   if (strcmp(argv[1], "-d") == 0) {
+      delete_set = argv[2];
+   } else {
+      delete_set = argv[1];
+   }
+   return delete_set;
+}
+
+void execute_delete_mode(char c, char *delete_set) {
+   if (char_in_delete_set(c, delete_set) == 0) {
+      putchar(c);
+   }
+}
+
 int main(int argc, char **argv) {
    if (argc != 3) {
-	printf("There are not enough arguments given\n");
-	return 1;
+	   printf("Program requires only 3 arguments, %d given\n", argc);
+	   return 1;
    }
-   int x = get_mode(argc, argv);
-   printf("%d\n", x);
+   int code = get_mode(argc, argv);
    int c;
    while ((c = getchar()) != EOF) {
-	putchar(c);
+      if (code == 1) {
+         char *delete_set = get_delete_set(argv);
+         execute_delete_mode(c, delete_set);
+      } else {
+
+      }
    }
    return 0;
 }
