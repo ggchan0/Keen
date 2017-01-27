5dc453af43c6c744adc0f71dc65f312b9fd14869
Garrett <ggchan@calpoly.edu>
2017-01-27 02:50:55 -0800

    lab3

--- CPE-357/lab3/readline.c
+++ CPE-357/lab3/readline.c
@@ -1,18 +1,49 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-char * readline(FILE *file) {
-   
+char * doubleSize(char *str, int length) {
+   char *new_str = malloc(sizeof(char) * length * 2);
+   int i;
+   for (i = 0; i < length; i++) {
+      new_str[i] = str[i];
+   }
+   free(str);
+   return new_str;
+}
+
+char *readline(FILE *file) {
+   int length = 80;
+   int index = 0;
+   char *str = malloc(sizeof(char) * length);
+   int c;
+   while ((c = fgetc(file)) != EOF && c != '\n') {
+      str[index++] = c;
+      if (index == length) {
+         length *= 2;
+         str = doubleSize(str, length);
+      }
+   }
+   if (c == EOF) {
+      str = NULL;
+   }
+   return str;
 }
 
 int main(void) {
    FILE *f = fopen("test.txt", "r");
-   int c;
    if (f == NULL) {
       printf("Error opening file\n");
       return 1;
    }
-   while ((c = fgetc(f)) != EOF) {
-      printf("%c", c);
+   while (1) {
+      char *str = readline(f);
+      if (str == NULL) {
+         free(str);
+         break;
+      } else {
+         printf("%s\n", str);
+         free(str);
+      }
    }
    return 0;
 }
