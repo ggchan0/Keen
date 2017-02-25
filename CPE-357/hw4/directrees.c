d7968716b24c6abff8a0e265362e1c788ce0edbc
Garrett <ggchan@calpoly.edu>
2017-02-24 23:06:40 -0800

    finished?

--- CPE-357/hw4/directrees.c
+++ CPE-357/hw4/directrees.c
@@ -73,47 +73,6 @@ void printDirs(char *dir, int level, int print_hidden, int print_perms) {
                         checkedChdir(cwd);
                      }
                   }
-                  /*
-                  if (isHidden(namelist[i]->d_name) && print_hidden) {
-                     printIndent(level);
-                     if (print_perms) {
-                        printPermissions(buf, type);
-                     }
-                     printf("--%s\n", namelist[i]->d_name);
-                     if (chdir(namelist[i]->d_name) == -1) {
-                        printf("Cannot change into directory %s\n", namelist[i]->d_name);
-                     } else {
-                        char new_cwd[1024];
-                        if (getcwd(new_cwd, sizeof(new_cwd)) == NULL) {
-                           fprintf(stderr, "Cannot get current working directory\n");
-                        }
-                        strcat(new_cwd, "/");
-                        printDirs(new_cwd, level + 1, print_hidden, print_perms);
-                        if (chdir(cwd) == -1) {
-                           printf("Cannot change into directory %s\n", cwd);
-                        }
-                     }
-                  } else if (!isHidden(namelist[i]->d_name)) {
-                     printIndent(level);
-                     if (print_perms) {
-                        printPermissions(buf, type);
-                     }
-                     printf("--%s\n", namelist[i]->d_name);
-                     if (chdir(namelist[i]->d_name) == -1) {
-                        printf("Cannot change into directory %s\n", namelist[i]->d_name);
-                     } else {
-                        char new_cwd[1024];
-                        if (getcwd(new_cwd, sizeof(new_cwd)) == NULL) {
-                           fprintf(stderr, "Cannot get current working directory\n");
-                        }
-                        strcat(new_cwd, "/");
-                        printDirs(new_cwd, level + 1, print_hidden, print_perms);
-                        if (chdir(cwd) == -1) {
-                           printf("Cannot change into directory %s\n", cwd);
-                        }
-                     }
-                  }
-                  */
                }
             }
          }
@@ -130,59 +89,6 @@ void executePrintDirs(char *dir, struct stat buf, char type, int print_hidden, i
    printf(" %s\n", dir);
    printDirs(".", 0, print_hidden, print_perms);
 }
-
-/*
-int main(int argc, char **argv) {
-   int print_hidden = 0, print_perms = 0;
-   int i;
-   struct stat buf;
-   char type;
-   char cwd[1024];
-   if (getcwd(cwd, sizeof(cwd)) == NULL) {
-      fprintf(stderr, "Cannot get current working directory\n");
-   }
-   strcat(cwd, "/");
-   for (i = 1; i < argc; i++) {
-      if (isFlag(argv[i])) {
-         print_hidden = print_hidden | checkHidden(argv[i]);
-         print_perms = print_perms | checkPerms(argv[i]);
-      } else {
-         if (isAbsolutePath(argv[i])) {
-            if (stat(argv[i], &buf) == -1) {
-               printf("%s does not exist\n", argv[i]);
-               continue;
-            }
-            if ((type = getFileTypeChar(buf)) != 'd') {
-               if (print_perms) {
-                  printPermissions(buf, type);
-               }
-               printf("%s", argv[i]);
-            } else {
-               if (chdir(argv[i]) == -1) {
-                  printf("Cannot change into directory %s\n", argv[i]);
-               } else {
-                  printf("%s\n", argv[i]);
-                  printDirs(".", 0, print_hidden, print_perms);
-               }
-            }
-         } else {
-            chdir(cwd);
-            if (chdir(argv[i]) == -1) {
-               printf("Cannot change into local directory %s\n", argv[i]);
-            } else {
-               printf("%s\n", argv[i]);
-               printDirs(".", 0, print_hidden, print_perms);
-            }
-         }
-      }
-   }
-   if (argc == 1) {
-      chdir(cwd);
-      printf("%s\n", cwd);
-      printDirs(cwd, 0, print_hidden, print_perms);
-   }
-   return 0;
-}*/
 
 int main(int argc, char **argv) {
    int print_hidden = 0, print_perms = 0, file_hit = 0;
